1c590825b6555daa339fa1db74f437831a15149a
Ryan Abel <ryan@talkabel.com>
2023-12-24 12:09:43 -0500

    Add commandline arguments

--- conto/conto.py
+++ conto/conto.py
@@ -5,6 +5,7 @@ A contact center receiving a variety of contacts (phone, email, chat) with a
 variety of skills (e.g., sales, support, billing) and a variety of agents with
 different skills.
 """
+import argparse
 import logging
 import random
 from enum import Enum
@@ -17,14 +18,43 @@ from contact_type import ContactType
 
 
 if __name__ == '__main__':
-    CONTACT_DEMAND_RATE = 2
-    MEAN_INTERARRIVAL_TIME = CONTACT_DEMAND_RATE / 60
-    AVG_HANDLE_TIME = 300
-    AVG_HOLD_TIME = 30
-    HOLD_PROBABILITY = 0.15
-    AVG_ABANDON_TIME = 120
-    AGENT_COUNT = 10
-    SIM_TIME = 2000
+    # Args
+    parser = argparse.ArgumentParser(description='Contact center simulator')
+    parser.add_argument('-cph', '--contacts-per-hour', type=int,
+                        help=('Contacts per hour. Calculated based on '
+                        'the number of agents if not specified.'),
+                        default=100)
+    parser.add_argument('--handle-time', type=int, help='Average handle time',
+                        default=300)
+    parser.add_argument('--hold-probability', type=float,
+                        help='Probability a contact is placed on hold',
+                        default=0.15)
+    parser.add_argument('--hold-time', type=int,
+                        help='Average time a contact is placed on hold',
+                        default=30)
+    parser.add_argument('--abandon-time', type=int,
+                        help='Average wait time before abandoning',
+                        default=120)
+    parser.add_argument('--agent-count', type=int, help='Number of agents',
+                        default=10)
+    parser.add_argument('--sim-time', type=int, help='Duration of simulation',
+                        default=10000)
+
+    args = parser.parse_args()
+    # TODO: Fix this calculation
+    if args.contacts_per_hour:
+        CONTACTS_PER_HOUR = args.contacts_per_hour / 60
+    else:
+        agent_capacity = ((3600 / args.handle_time) * args.agent_count) * 0.2
+        CONTACTS_PER_HOUR = agent_capacity / 60
+
+    MEAN_INTERARRIVAL_TIME = CONTACTS_PER_HOUR / 60
+    AVG_HANDLE_TIME = args.handle_time
+    AVG_HOLD_TIME = args.hold_time
+    HOLD_PROBABILITY = args.hold_probability
+    AVG_ABANDON_TIME = args.abandon_time
+    AGENT_COUNT = args.agent_count
+    SIM_TIME = args.sim_time
 
     # Logging
     logging.basicConfig(level=logging.INFO)
