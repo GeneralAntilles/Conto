bb64cd57e9ca266ec3d472535ce8b27d83c5c822
Ryan Abel <ryan@talkabel.com>
2023-12-24 11:45:52 -0500

    Add calculated attributes

--- conto/contact_statistics.py
+++ conto/contact_statistics.py
@@ -15,12 +15,17 @@ class ContactStatistics:
     Tracks contact statistics for a contact center.
 
     Attributes:
+
         count (int): Number of contacts
         abandoned (int): Number of abandoned contacts
+        abandonment_rate (float): Abandonment rate
         duration (int): Total duration of contacts (seconds)
+        aht (float): Average handle time (seconds)
         hold_count (int): Number of holds
         hold_duration (int): Total duration of holds (seconds)
+        avg_hold_time (float): Average hold time (seconds)
         wait_time (int): Total wait time (seconds)
+        asa (float): Average speed of answer (seconds)
     """
     def __init__(self):
         self.logger = logging.getLogger(__name__)
@@ -47,12 +52,33 @@ class ContactStatistics:
         self.hold_duration += contact.hold_duration
         self.wait_time += contact.wait_time
 
+    @property
+    def abandonement_rate(self):
+        """Abandonement rate"""
+        return self.abandoned / self.count if self.count > 0 else 0
+
+    @property
+    def aht(self):
+        """Average handle time"""
+        return self.duration / self.count if self.count > 0 else 0
+
+    @property
+    def avg_hold_time(self):
+        """Average hold time"""
+        return self.hold_duration / self.hold_count if self.hold_count > 0 else 0
+
+    @property
+    def asa(self):
+        """Average speed of answer"""
+        return self.wait_time / self.count if self.count > 0 else 0
+
     def __str__(self):
         return (
             f'Contacts: {self.count} '
-            f'Abandoned: {self.abandoned} '
-            f'AHT: {self.duration / self.count:0.0f}s '
+            f'Handled: {self.count - self.abandoned} '
+            f'Abandonment rate: {self.abandonement_rate:0.2%} '
+            f'AHT: {self.aht:0.0f}s '
             f'Holds: {self.hold_count} '
-            f'Avg hold time: {self.hold_duration / self.hold_count:0.0f}s '
-            f'ASA: {self.wait_time / self.count:0.0f}s'
+            f'Avg hold time: {self.avg_hold_time:0.0f}s '
+            f'ASA: {self.asa:0.0f}s'
         )
